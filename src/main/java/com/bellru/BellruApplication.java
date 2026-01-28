cbec8d4030f4a0295d4441b8735ba6b2499c65dd
Thewni03 <thewni2003@gmail.com>
2026-01-28 10:00:00 +0530

    Create main Spring Boot application class

---
+++ src/main/java/com/bellru/BellruApplication.java
@@ -0,0 +1,11 @@
+package com.bellru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellruApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BellruApplication.class, args);
+    }
+}
